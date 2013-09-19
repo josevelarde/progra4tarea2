b480823c94062e74feeae688f5f664b7049b6824
Josevelarde <pepe91_1308@hotmail.com>
2013-09-18 21:11:09 -0500

    tarea

---
+++ tarea2/tarea2.php
@@ -0,0 +1,11 @@
+<html>
+ <head></head>
+ <body>
+ <?php
+echo "Dolares a Cambiar ".$Dolares=23;
+echo "Tasa de Cambio ".$TasaCambio=15.5;
+$resultado=$Dolares*$TasaCambio;
+echo "Pesos Mexicanos:".$resultado;
+ ?>
+ </body>
+ </html>
